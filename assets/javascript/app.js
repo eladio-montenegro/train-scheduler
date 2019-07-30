c3204ea79c7ba7d442588e242e19e05327e5cedb
Eladio Montenegro <eladio.montenegro@gmail.com>
2019-07-29 22:26:43 -0500

    created the firebase database and added the code to the js doc

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+//setup firebase database
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCsDYMyTtmpUxv_WnqCIKqbtl8tHr4WBNg",
+    authDomain: "train-scheduler-671e3.firebaseapp.com",
+    databaseURL: "https://train-scheduler-671e3.firebaseio.com",
+    projectId: "train-scheduler-671e3",
+    storageBucket: "train-scheduler-671e3.appspot.com",
+    messagingSenderId: "1036719751741",
+    appId: "1:1036719751741:web:f62107e67722a53c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+//collect info on click
+
+
+
+//get the input values
+
+
+
+
+
+//
+
+
+
+
+
+
+//moment.js stuff.  This part is going to be rough 
+
+
+
+
